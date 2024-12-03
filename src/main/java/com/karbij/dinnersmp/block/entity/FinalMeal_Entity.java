b6cbcb0b3cca5c18c790894b146d1db21c3d22b3
Talmanaze <j.zyborg@gmail.com>
2024-12-03 13:55:47 -0500

    Continued Progress

--- src/main/java/com/karbij/dinnersmp/block/entity/FinalMeal_Entity.java
+++ src/main/java/com/karbij/dinnersmp/block/entity/FinalMeal_Entity.java
@@ -4,40 +4,40 @@ import net.minecraft.core.BlockPos;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
 import software.bernie.geckolib.animatable.GeoBlockEntity;
-import software.bernie.geckolib.core.animatable.GeoAnimatable;
+import software.bernie.geckolib.constant.DefaultAnimations;
 import software.bernie.geckolib.core.animatable.instance.AnimatableInstanceCache;
-import software.bernie.geckolib.core.animatable.instance.SingletonAnimatableInstanceCache;
 import software.bernie.geckolib.core.animation.AnimatableManager;
-import software.bernie.geckolib.core.animation.Animation;
 import software.bernie.geckolib.core.animation.AnimationController;
-import software.bernie.geckolib.core.animation.AnimationState;
-import software.bernie.geckolib.core.animation.RawAnimation;
-import software.bernie.geckolib.core.animation.AnimatableManager.ControllerRegistrar;
-import software.bernie.geckolib.core.object.PlayState;
+import software.bernie.geckolib.util.GeckoLibUtil;
 import software.bernie.geckolib.util.RenderUtils;
 
 public class FinalMeal_Entity extends BlockEntity implements GeoBlockEntity {
 
-    private AnimatableInstanceCache cache = new SingletonAnimatableInstanceCache(this);
+    private final AnimatableInstanceCache cache = GeckoLibUtil.createInstanceCache(this);
 
-    public FinalMeal_Entity( BlockPos pPos, BlockState pBlockState) {
-      super(ModBlockEntities.FINAL_MEAL_ENTITY.get(), pPos, pBlockState);
-   }
+	public FinalMeal_Entity(BlockPos pos, BlockState state) {
+		super(ModBlockEntities.FINAL_MEAL_ENTITY.get(), pos, state);
+	}
 
-    @Override
-    public void registerControllers(ControllerRegistrar controllerRegistrar) {
-        controllerRegistrar.add(new AnimationController<>(this, "controller", 0, this::predicate));
-    }
+	// We just want a permanent idle animation happening here
+	// But if it's day time we want him to take a nap
+	@Override
+	public void registerControllers(AnimatableManager.ControllerRegistrar controllers) {
+		controllers.add(new AnimationController<>(this, state -> {
+//			if (getLevel().getDayTime() > 23000 || getLevel().getDayTime() < 13000) {
+//				return state.setAndContinue(DefaultAnimations.REST);
+//			}
+//			else {
+				return state.setAndContinue(DefaultAnimations.IDLE);
+//			}
+		}));
+	}
 
-    private <T extends GeoAnimatable> PlayState predicate(AnimationState<T> tAnimationState) {
-        tAnimationState.getController().setAnimation(RawAnimation.begin().then("idle", Animation.LoopType.LOOP));
-        return PlayState.CONTINUE;
-    }
+	@Override
+	public AnimatableInstanceCache getAnimatableInstanceCache() {
+		return this.cache;
+	}
 
-    @Override
-    public AnimatableInstanceCache getAnimatableInstanceCache() {
-        return cache;
-    }
     @Override
     public double getTick(Object blockEntity) {
         return RenderUtils.getCurrentTick();
--- src/main/java/com/karbij/dinnersmp/block/entity/client/FinalMealModel.java
+++ src/main/java/com/karbij/dinnersmp/block/entity/client/FinalMealModel.java
@@ -4,23 +4,14 @@ import com.karbij.dinnersmp.DinnerSMP;
 import com.karbij.dinnersmp.block.entity.FinalMeal_Entity;
 
 import net.minecraft.resources.ResourceLocation;
+import software.bernie.geckolib.model.DefaultedBlockGeoModel;
 import software.bernie.geckolib.model.GeoModel;
 
-public class FinalMealModel extends GeoModel<FinalMeal_Entity> {
+public class FinalMealModel extends DefaultedBlockGeoModel<FinalMeal_Entity> {
 
-    @Override
-    public ResourceLocation getModelResource(FinalMeal_Entity animatable) {
-        return new ResourceLocation(DinnerSMP.MODID, "geo/finalmeal.geo.json");
-    }
 
-    @Override
-    public ResourceLocation getTextureResource(FinalMeal_Entity animatable) {
-        return new ResourceLocation(DinnerSMP.MODID, "textures/block/finalmeal_texture.png");
-    }
-
-    @Override
-    public ResourceLocation getAnimationResource(FinalMeal_Entity animatable) {
-        return new ResourceLocation(DinnerSMP.MODID, "animations/final_meal.animation.json");
+    public FinalMealModel() {
+        super(new ResourceLocation(DinnerSMP.MODID, "finalmeal"));
     }
 
 }
